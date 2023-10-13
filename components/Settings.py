caf78246e5303bef0e2cb0e8534defad0055aa12
Goliaten <59554399+Goliaten@users.noreply.github.com>
2023-10-13 12:03:29 +0200

    Destroy safeguard and content grounwork
    Safeguard for destroy to try to destroy the window in case it was not ever started. And a frame for a framework to make settings window make itself automatically, based on the settings available.

--- components/Settings.py
+++ components/Settings.py
@@ -6,8 +6,22 @@ class Settings:
         
         # https://tkdocs.com/tutorial/windows.html#wm
         
-        dlg = Toplevel(root)
+        self.string_vars = {}
+        self.root = root
+        self.created = False
+        self.allowed_widgets = ["radio", "checkbox", "combobox"]
+        self.settings = {}#------------------------------------------------------------
+        #have predefined structure or load structure from file for settings?
+        #prob have it predefined with default variables, and overwrite them with settings, to prevent any missing
+        #but then again, predefined in here, or in separate file?
+        #in separate file easier for management, here more "secure"
+        #take separate file approach
+     
+    def create(self):
+        
+        dlg = Toplevel(self.root)
         self.dlg = dlg
+        self.frame = ttk.Frame(self.dlg)
         
         dlg.title("Settings")
         dlg.geometry("200x200")
@@ -15,17 +29,30 @@ class Settings:
         dlg.protocol("WM_DELETE_WINDOW", self.destroy)
         dlg.attributes("-topmost", 1)
         
-        dlg.iconify()
-     
-    def create(self):
-        pass
+        self.created = True
     
     def destroy(self):
-        self.dlg.grab_release()
-        self.dlg.destroy()
+        try:
+            self.dlg.grab_release()
+            self.dlg.destroy()
+            self.created = False
+        except Exception as e:
+            print(e)
+    
+    def add_field(self, widg_type, name, caption):
+        
+        if widg_type not in self.allowed_widgets:
+            print(f"Err - {widg_type} not in allowed widgets. {name} {caption}")
+            return
+        
+        if self.created:
+            if widg_type == "radio":
+                pass #--------------------------------------------------------
+            else:
+                print("not implemented yet")
     
     def load_settings():
-        pass
+        pass#--------------------------------------------------------------
     
     def save_settings():
         pass
